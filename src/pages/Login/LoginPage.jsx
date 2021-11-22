63891b926b5e02c5419865400339abfbd0865b92
Mylena-Rodrigues <mylenawz@yahoo.com.br>
2021-11-22 00:16:17 -0300

    Organize imports LoginPage

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,25 +1,30 @@
+import * as Api from "../../api/auth";
+
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useUsers } from "../../contexts/userContext";
+
 import { LoginPageStyle } from "./LoginPage";
 import { PageStyle } from "../LoginAndRegisterPages";
-import * as Api from "../../api/auth";
+
 import ButtonSubmit from "../../components/ButtonSubmit/ButtonSubmit.jsx";
 import ForgotPasswordModal from "../../components/ForgotPasswordModal/ForgotPasswordModal.jsx";
-import { useUsers } from "../../contexts/userContext";
+
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { isLogged, setIsLogged } = useUsers();
+  const { setIsLogged } = useUsers();
 
   const onSubmit = async (e) => {
     e.preventDefault();
     try {
       const response = await Api.signIn({ email, password });
-      console.log(response);
-      if (response.id) {
-        localStorage.setItem("user_id", response.id);
+      setIsLogged(true)
+      
+      if(response){
+        localStorage.setItem("@lendit/user_id", JSON.parse(response.id));
+        window.location.href = "/board";
       }
-      window.location.href = "/board";
-      setIsLogged(true);
     } catch (error) {
       alert("Erro ao efetuar login!");
     }
@@ -63,7 +68,7 @@ export default function LoginPage() {
               </div>
             </form>
             <p className="redirect">
-              Ainda não tem uma conta? <a href="/"> Registre-se </a>.
+              Ainda não tem uma conta? <Link to="/register"> Registre-se </Link>.
             </p>
           </div>
         </LoginPageStyle>
