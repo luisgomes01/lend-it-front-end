89459b4754feff1768e04f62ec2a4574d99d2a43
Mylena-Rodrigues <mylenawz@yahoo.com.br>
2021-11-22 00:15:10 -0300

    Organize components

--- src/pages/EmptyState/EmptyState.js
+++ src/pages/EmptyState/EmptyState.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.main`
 width: 100%;
 height: 100vh;
+position: absolute;
 display: flex;
 flex-direction: column;
 align-items: center;
