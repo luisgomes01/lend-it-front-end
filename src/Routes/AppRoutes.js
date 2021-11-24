e33de3a6d6e10084c6a9aa46f961215a6f454b51
Mylena-Rodrigues <mylenawz@yahoo.com.br>
2021-11-24 12:24:18 -0300

    Call ProfilePage

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,21 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+import "../global.css";
+
 import Menu from "../components/Menu/Menu.jsx";
 import Footer from "../components/Footer/Footer.jsx";
-import WelcomePage from "../../src/pages/Welcome/WelcomePage.jsx";
-import LoginPage from "../pages/Login/LoginPage.jsx";
-import RegisterPage from "../pages/Register/RegisterPage.jsx";
-import PerfilPage from "../pages/Perfil/PerfilPage.jsx";
-import LoanItem from "../components/LoanItem/LoanItem.jsx";
-import LateItem from "../components/LateItem/LateItem.jsx";
-// import {useState, useEffect} from 'react'
-import "../global.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import ProfilePage from "../pages/Profile/ProfilePage.jsx";
 import EmptyState from "../pages/EmptyState/EmptyState.jsx";
-import AddButton from "../components/AddButton/AddButton.jsx";
-import ListLoanItemBorrow from "../components/ListComponents/ListBorrow/ListLoanItemBorrow.jsx";
-import ListLateItemBorrow from "../components/ListComponents/ListBorrow/ListLateItemBorrow.jsx";
-import ListLoanItemLoan from "../components/ListComponents/ListLoan/ListLoanItemLoan.jsx";
-import ListLateItemLoan from "../components/ListComponents/ListLoan/ListLateItemLoan.jsx";
 
 export default function AppRoutes() {
   return (
@@ -23,10 +13,9 @@ export default function AppRoutes() {
       <Menu />
       <Routes>
         <Route path="/board" element={<EmptyState />} />
-        <Route path="/profile" element={<PerfilPage />} />
-        <Route path="/emprestado" element={<ListLoanItemBorrow />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/emprestado" element={<EmptyState />} />
       </Routes>
-
       <Footer />
     </>
   );
